9db8a7587751d38a18401aa0616c4ef141ad0619
Andreyrvs <cortaesai@gmail.com>
2021-10-18 18:24:22 -0300

    Requisito 04

--- src/challenges.js
+++ src/challenges.js
@@ -21,10 +21,10 @@ function splitSentence(a) {
 }
 
 // Desafio 4
-function concatName() {
+function concatName(a) {
   // seu código aqui
+  return ((a[a.length - 1]) + ', ' + a[0]);
 }
-
 // Desafio 5
 function footballPoints(a, b) {
   // seu código aqui
@@ -37,19 +37,43 @@ function footballPoints(a, b) {
 }
 
 // Desafio 6
-function highestCount() {
+function highestCount(a) {
   // seu código aqui
+  let maior = a[0];
+  for (let index = 0; index < a.length; index += 1) {
 
+    for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+      if (a[index] > a[secondIndex]) {
+        maior += 1;
+        return maior;
+      }
+      return a;
+    }
+  }
 }
 
 // Desafio 7
-function catAndMouse() {
+function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
+  if (cat1 < mouse && cat1 < cat2) {
+    console.log(cat1);
+  } else if (cat2 < mouse && cat2 < cat1) {
+    console.log(cat2);
+  } else {
+    console.log('os gatos trombam e o rato foge');
+  }
 }
 
 // Desafio 8
-function fizzBuzz() {
+function fizzBuzz(a) {
   // seu código aqui
+  let retornaArray;
+  for (let index = 0; index < a.length; index += 1) {
+    if (a[index] ) {
+      
+    }
+    
+  }
 }
 
 // Desafio 9
