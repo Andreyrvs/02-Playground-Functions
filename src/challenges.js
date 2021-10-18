2786714e3c462f7d0bfcfc35bb5e287b0c3e3346
Andreyrvs <cortaesai@gmail.com>
2021-10-18 15:59:51 -0300

    Requisito - 02

--- src/challenges.js
+++ src/challenges.js
@@ -7,8 +7,11 @@ function compareTrue(a, b) {
 }
 
 // Desafio 2
-function calcArea() {
+function calcArea(base, height) {
   // seu código aqui
+  let area;
+  area = (base * height) / 2;
+  return area;
 }
 
 // Desafio 3
